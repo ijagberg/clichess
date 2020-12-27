ff70fbb9213877d6175d87f6f06a07cee4e4cfec
Isak Jägberg <ijagberg@gmail.com>
2020-12-27 23:15:56 +0100

    naive

--- server/src/server.rs
+++ server/src/server.rs
@@ -23,17 +23,7 @@ impl Server {
             .unwrap();
 
         loop {
-            // Asynchronously wait for an inbound socket.
             let (mut socket, _) = listener.accept().await.unwrap();
-
-            // And this is where much of the magic of this server happens. We
-            // crucially want all clients to make progress concurrently, rather than
-            // blocking one on completion of another. To achieve this we use the
-            // `tokio::spawn` function to execute the work in the background.
-            //
-            // Essentially here we're executing a new task to run concurrently,
-            // which will allow all of our clients to be processed concurrently.
-
             task::spawn(async move {
                 let mut buf = vec![0; 1024];
                 loop {
@@ -47,8 +37,8 @@ impl Server {
                 }
 
                 if let Ok(content) = String::from_utf8(buf) {
-                    let mut trimmed = content.trim();
-                    trimmed = &trimmed[..trimmed.len() - 1];
+                    let (first, last) = (content.find('{').unwrap(), content.rfind('}').unwrap());
+                    let trimmed = &content[first..=last];
                     println!("'{}'", trimmed);
                     let message: ChessMessage = serde_json::from_str(trimmed).unwrap();
                     match message {
