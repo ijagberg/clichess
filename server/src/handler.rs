9f5aa82b3a38146d16b997b1d52e1a41451b780a
Isak Jägberg <ijagberg@gmail.com>
2020-12-31 20:25:57 +0100

    Copy ws example from git

---
+++ server/src/handler.rs
@@ -0,0 +1,53 @@
+use crate::{ws, Client, Clients, Result};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+use warp::{http::StatusCode, reply::json, ws::Message, Reply};
+
+#[derive(Deserialize, Debug)]
+pub struct ConnectRequest {
+    room_id: String,
+}
+
+#[derive(Serialize, Debug)]
+pub struct ConnectResponse {
+    room_id: String,
+    token: String,
+}
+
+pub async fn register_handler(body: ConnectRequest, clients: Clients) -> Result<impl Reply> {
+    let uuid = Uuid::new_v4();
+
+    register_client(uuid, body.room_id.clone(), clients).await;
+    Ok(json(&ConnectResponse {
+        room_id: body.room_id,
+        token: "isak".to_string(),
+    }))
+}
+
+async fn register_client(user_id: Uuid, room_id: String, clients: Clients) {
+    clients.write().await.insert(
+        user_id.clone(),
+        Client {
+            user_id,
+            room_id,
+            sender: None,
+        },
+    );
+}
+
+pub async fn unregister_handler(id: Uuid, clients: Clients) -> Result<impl Reply> {
+    clients.write().await.remove(&id);
+    Ok(StatusCode::OK)
+}
+
+pub async fn ws_handler(ws: warp::ws::Ws, id: Uuid, clients: Clients) -> Result<impl Reply> {
+    let client = clients.read().await.get(&id).cloned();
+    match client {
+        Some(c) => Ok(ws.on_upgrade(move |socket| ws::client_connection(socket, id, clients, c))),
+        None => Err(warp::reject::not_found()),
+    }
+}
+
+pub async fn health_handler() -> Result<impl Reply> {
+    Ok(StatusCode::OK)
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,29 +1,61 @@
-mod server;
+use std::collections::HashMap;
+use std::convert::Infallible;
+use std::sync::Arc;
+use tokio::sync::{mpsc, RwLock};
+use uuid::Uuid;
+use warp::{ws::Message, Filter, Rejection};
 
-use serde::{Deserialize, Serialize};
-use structopt::StructOpt;
+mod handler;
+mod ws;
 
-#[derive(StructOpt, Debug)]
-struct Opts {
-    #[structopt(long)]
-    port: u32,
+type Result<T> = std::result::Result<T, Rejection>;
+type Clients = Arc<RwLock<HashMap<Uuid, Client>>>;
+
+#[derive(Debug, Clone)]
+pub struct Client {
+    pub user_id: Uuid,
+    pub room_id: String,
+    pub sender: Option<mpsc::UnboundedSender<std::result::Result<Message, warp::Error>>>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-enum ChessMessage {
-    Connect(ConnectMessage),
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct ConnectMessage {
-    room: String,
-}
-
-#[async_std::main]
+#[tokio::main]
 async fn main() {
-    dotenv::dotenv().ok();
-    let opts = Opts::from_args();
+    let clients: Clients = Arc::new(RwLock::new(HashMap::new()));
 
-    let runner = server::Server::new();
-    runner.run(opts.port).await;
+    let health_route = warp::path!("health").and_then(handler::health_handler);
+
+    let register = warp::path("register");
+    let register_routes = register
+        .and(warp::post())
+        .and(warp::body::json())
+        .and(with_clients(clients.clone()))
+        .and_then(handler::register_handler)
+        .or(register
+            .and(warp::delete())
+            .and(warp::path::param())
+            .and(with_clients(clients.clone()))
+            .and_then(handler::unregister_handler));
+
+    // let publish = warp::path!("publish")
+    //     .and(warp::body::json())
+    //     .and(with_clients(clients.clone()))
+    //     .and_then(handler::publish_handler);
+
+    let ws_route = warp::path("ws")
+        .and(warp::ws())
+        .and(warp::path::param())
+        .and(with_clients(clients.clone()))
+        .and_then(handler::ws_handler);
+
+    let routes = health_route
+        .or(register_routes)
+        .or(ws_route)
+        // .or(publish)
+        .with(warp::cors().allow_any_origin());
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+}
+
+fn with_clients(clients: Clients) -> impl Filter<Extract = (Clients,), Error = Infallible> + Clone {
+    warp::any().map(move || clients.clone())
 }
